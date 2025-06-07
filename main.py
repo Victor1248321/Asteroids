5a7b299a938c5c9b01e691e268969b587b51e94b
Master-Magician <Marvin1248@hotmail.com>
2025-06-07 09:26:51 -0500

    Added Shootable Projectiles

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from projectile import Shot
+
 def main():
     pygame.init
     game_clock = pygame.time.Clock()
@@ -11,9 +13,11 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = updatable
+    Shot.containers = (shots, updatable, drawable)
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
     print("Starting Asteroids!")
