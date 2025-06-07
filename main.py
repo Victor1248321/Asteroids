bb18103111fa1f5f95a61003b2f9b9252c5580c5
Master-Magician <Marvin1248@hotmail.com>
2025-06-07 10:50:47 -0500

    Asteroids Split on Hit

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def main():
             for shot in shots:
                 if asteroid.collide(shot):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
 
 
